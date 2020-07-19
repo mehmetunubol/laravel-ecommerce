808a965b2652a6f16346f64324822e24e6c3b37b
Mehmet Unubol <mehmet.unubol@redpine.com.tr>
2020-07-19 18:16:23 +0300

    ProductImage upload/delete functionality added

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -8,16 +8,9 @@
             </a>
         </li>
         <li>
-            <a class="app-menu__item {{ Route::currentRouteName() == 'admin.settings' ? 'active' : '' }}" href="{{ route('admin.settings') }}">
-                <i class="app-menu__icon fa fa-cogs"></i>
-                <span class="app-menu__label">Settings</span>
-            </a>
-        </li>
-        <li>
-            <a class="app-menu__item {{ Route::currentRouteName() == 'admin.categories.index' ? 'active' : '' }}"
-                href="{{ route('admin.categories.index') }}">
-                <i class="app-menu__icon fa fa-tags"></i>
-                <span class="app-menu__label">Categories</span>
+            <a class="app-menu__item {{ Route::currentRouteName() == 'admin.products.index' ? 'active' : '' }}" href="{{ route('admin.products.index') }}">
+                <i class="app-menu__icon fa fa-shopping-bag"></i>
+                <span class="app-menu__label">Products</span>
             </a>
         </li>
         <li>
@@ -27,9 +20,21 @@
             </a>
         </li>
         <li>
+            <a class="app-menu__item {{ Route::currentRouteName() == 'admin.categories.index' ? 'active' : '' }}" href="{{ route('admin.categories.index') }}">
+                <i class="app-menu__icon fa fa-tags"></i>
+                <span class="app-menu__label">Categories</span>
+            </a>
+        </li>
+        <li>
             <a class="app-menu__item {{ Route::currentRouteName() == 'admin.attributes.index' ? 'active' : '' }}" href="{{ route('admin.attributes.index') }}">
                 <i class="app-menu__icon fa fa-th"></i>
                 <span class="app-menu__label">Attributes</span>
+            </a>
+        </li>
+        <li>
+            <a class="app-menu__item {{ Route::currentRouteName() == 'admin.settings' ? 'active' : '' }}" href="{{ route('admin.settings') }}">
+                <i class="app-menu__icon fa fa-cogs"></i>
+                <span class="app-menu__label">Settings</span>
             </a>
         </li>
     </ul>
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,6 +15,7 @@ const mix = require('laravel-mix');
 // to copy our assets from the resources foldet to public folder.
 mix.copyDirectory('resources/admintemplate', 'public/admintemplate');
 mix.copyDirectory('resources/customertemplate', 'public/customertemplate');
+mix.copyDirectory('storage/app/public/products', 'public/storage/products');
 
 /* use in the future to compile our VueJS Components
 mix.js('resources/js/app.js', 'public/js')
