8ecf20f9620cc6addb23d5a6cf1fe08c3c450fbe
Mehmet Unubol <mehmet.unubol@redpine.com.tr>
2020-07-20 15:18:03 +0300

    Order customer side is implemented without fully integrated payment

--- app/Http/Controllers/Site/CheckoutController.php
+++ app/Http/Controllers/Site/CheckoutController.php
@@ -23,9 +23,35 @@ class CheckoutController extends Controller
     public function placeOrder(Request $request)
     {
         // Before storing the order we should implement the
-        // request validation which I leave it to you
+        // request validation TODO
         $order = $this->orderRepository->storeOrderDetails($request->all());
 
-        dd($order);
+        // You can add more control here to handle if the order
+        // is not stored properly
+        if ($order) {
+            dd($order);
+            // TODO: Payment
+            # $this->payPal->processPayment($order);
+        }
+
+        return redirect()->back()->with('message','Order not placed');
+    }
+
+    public function complete(Request $request)
+    {
+        // TODO: Payment
+        /* $paymentId = $request->input('paymentId');
+        $payerId = $request->input('PayerID');
+        $status = $this->payPal->completePayment($paymentId, $payerId);
+        */
+
+        $order = Order::where('order_number', $status['invoiceId'])->first();
+        $order->status = 'processing';
+        $order->payment_status = 1;
+        $order->payment_method = 'PayPal -'.// TODO: Payment $status['salesId'];
+        $order->save();
+
+        Cart::clear();
+        return view('site.pages.success', compact('order'));
     }
 }
--- resources/views/site/partials/header.blade.php
+++ resources/views/site/partials/header.blade.php
@@ -51,12 +51,14 @@
                                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                         {{ Auth::user()->full_name }} <span class="caret"></span>
                                     </a>
+                                    
                                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                         <a class="dropdown-item" href="{{ route('logout') }}"
                                            onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                             {{ __('Logout') }}
                                         </a>
+                                        <a class="dropdown-item" href="{{ route('account.orders') }}">Orders</a>
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                             @csrf
                                         </form>
