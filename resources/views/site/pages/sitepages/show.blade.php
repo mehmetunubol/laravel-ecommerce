f7921bac721a85d03e76888b57d635eb08954cfb
Mehmet Unubol <mehmet.unubol@redpine.com.tr>
2020-09-16 14:57:01 +0300

    SitePages Generic Implementation is improved for front-end integration

--- resources/views/site/pages/sitepages/show.blade.php
+++ resources/views/site/pages/sitepages/show.blade.php
@@ -1,1 +1,11 @@
-<?php dd($sitepage) ?>
+@extends('site.app')
+@section('title', 'Login')
+@section('content')
+<div class="body">
+   <div role="main" class="main">
+      {!! $sitepage->content !!}
+      @include("site.sections.our_catalogue")
+   </div>
+   @include("site.partials.footer_lite")
+</div>
+@stop
