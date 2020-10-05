6868fcffb6bd0cf2b25aa66e206cb4c5a8f90e83
mehmetunubol <mehmetunubol@gmail.com>
2020-10-05 14:38:21 +0300

    General Attribute Filter Implementation - Backend

--- app/Http/Controllers/Site/CategoryController.php
+++ app/Http/Controllers/Site/CategoryController.php
@@ -36,7 +36,7 @@ class CategoryController extends Controller
             "/category/{slug}?filter="
         
         */
-        $filter = null;
+        $final_filter = [];
 
         /**
          * Price Filter Implementation
@@ -48,7 +48,7 @@ class CategoryController extends Controller
          */
         if ($request->has('price'))
         {
-            $filter = [];
+            
             $price_filters = explode('-', $request->input('price'));
             if(sizeof($price_filters) <= 0 && sizeof($price_filters) > 1)
             {
@@ -57,29 +57,39 @@ class CategoryController extends Controller
             foreach ($price_filters as $i => $price) {
                 if($i === 0)
                 {
-                    array_push($filter, ['price', '>', $price]);
+                    array_push($final_filter, ['price', '>', $price]);
                 }
                 else {
-                    array_push($filter, ['price', '<', $price]);
+                    array_push($final_filter, ['price', '<', $price]);
                 }
             }
         }
         // End of Price Filter Implementation
 
+        /**
+         * General Attribute Filter Implementation
+         *  
+         * Usage:    filter=attr1_name:attr1_val1-attr1_val2,attr2_name:attr2_val1-attr2_val2
+         * 
+         * Example: filter=Renk:kırmızı-beyaz,Beden:36-42-32
+         * 
+         */
+        
         $is_sidebar_on = $request->has('sb') ? true : false;
         if ($request->has('filter'))
         {
             $filters = explode(',', $request->input('filter'));
-            // TODO: It should be implemented according to decision
+            foreach ($filters as $filter) {
+                [$attribute, $values_string] = explode(':', $filter);
+                // TODO: Check if needed.
+                // -> $attribute: attribute name(Renk) is not used !! Since we have attribute value(kırmızı) as column in product_attributes table.
+                $values = explode('-', $values_string);
+                foreach ($values as $value) {
+                    // TODO: These should be Added to whole query as AND condition, but it should be OR in this foreach.
+                    array_push($final_filter, ['value', strval($value)]);
+                }
+            }
         }
-        // Test code, TODO: remove the test code
-        /*
-        $filter = [
-            ['price', '>', 1],
-            ['quantity', '>', 1]
-        ];
-        */
-        // End Test code
 
         /* 
             Below is the "Multiple Category Request" implementation.
@@ -87,13 +97,13 @@ class CategoryController extends Controller
             Example: www.site.com/category/man+woman+prom-dress
         */
         $cat_slugs = explode('+', $slug);
-        $category = $this->categoryRepository->findBySlugWithOrderFilter($cat_slugs[0], $order, $filter);
+        $category = $this->categoryRepository->findBySlugWithOrderFilter($cat_slugs[0], $order, $final_filter);
         $products = $category->products;
 
         foreach ($cat_slugs as $i => $cat_slug) {
             if( $i > 0)
             {
-                $products = $this->categoryRepository->findBySlugWithOrderFilter($cat_slugs[$i], $order, $filter)
+                $products = $this->categoryRepository->findBySlugWithOrderFilter($cat_slugs[$i], $order, $final_filter)
                     ->products->merge($products);
             }
         }
--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -156,42 +156,67 @@ class CategoryRepository extends BaseRepository implements CategoryContract
 
     public function findBySlugWithOrderFilter($slug, $order = null, $filter = null)
     {
-        if (isset($order) &&  isset($filter))
-        {
-            $closure = function ($products) use($order, $filter) 
-                        {
-                            return $products->orderBy($order['column'], $order['type'])
-                                            ->where($filter); 
-                        };
+        /**
+         * Seperation of the product and attribute filters
+         * 
+         */
+        $product_filter = [];
+        $attr_filter = [];
+        foreach ($filter as $each_filter) {
+            if($each_filter[0] === 'price')
+            {
+                array_push($product_filter, $each_filter);
+            }
+            else
+            {
+                array_push($attr_filter, $each_filter);
+            }
         }
-        else if (isset($order))
+        // End of Seperation of the product and attribute filters
+
+        /**
+         * Preperation of the closure which will be used in Model query.
+         */
+        $closure = function ($products) use($order, $product_filter, $attr_filter) 
         {
-            $closure = function ($products) use($order) 
-                        { 
-                            return $products->orderBy($order['column'], $order['type']); 
-                        };
-        }
-        else if (isset($filter))
-        {
-            $closure = function ($products) use($filter) 
-                        { 
-                            /* Note: If you change default sorting of the products you should change here also. */
-                            return $products->orderBy('order', 'desc')->orderBy('id', 'desc')->where($filter); 
-                        };
-        }
-        else
-        {
-            /* 
-                It is default order of products. It is configurable from admin panel.
-                It will be applicable if and only if 'customer' does not select any order.
-                If there is no configurable value for the column 'order', products will be sorted by id desc.
-                Note: If you change below, you should change the same in else-if block above.
-            */
-            $closure = function ($products) use($filter) 
-                        { 
-                            return $products->orderBy('order', 'desc')->orderBy('id', 'desc');
-                        };
-        }
+            $return_value;
+            if(isset($order))
+            {
+                $return_value = $products->orderBy($order['column'], $order['type']);
+            }
+            else
+            {
+                /* 
+                    It is default order of products. It is configurable from admin panel.
+                    It will be applicable if and only if 'customer' does not select any order.
+                    If there is no configurable value for the column 'order', products will be sorted by id desc.
+                */
+                $return_value = $products->orderBy('order', 'desc')->orderBy('id', 'desc');
+            }
+
+            if ($product_filter)
+            {
+                $return_value = $return_value->where($product_filter);
+            }
+
+            if($attr_filter)
+            {
+                $return_value = $return_value->whereHas('attributes' , 
+                                                    function ($attrs) use($attr_filter)
+                                                    {
+                                                        $attr_returned_val;
+                                                        foreach ($attr_filter as $i => $value) {
+                                                            if($i === 0)
+                                                            {
+                                                                $attr_returned_val = $attrs->where($value[0], $value[1]);
+                                                            }
+                                                            $attr_returned_val = $attrs->OrWhere($value[0], $value[1]);
+                                                        }
+                                                        return $attr_returned_val;
+                                                    }); 
+            }
+        };
+        // End of Preperation of the closure
 
         return Category::with(['products' => $closure])
             ->where('slug', $slug)
